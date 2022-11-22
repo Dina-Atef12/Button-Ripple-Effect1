2fc6f1883f811737c3eedbbb16104ede0a8d2c60
Dina-Atef12 <dinaatef684@gmail.com>
2022-11-22 02:08:27 +0200

    y=vertical    mouse pointers

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ btnEl.addEventListener("mouseover", (event) => {
   const x = event.pageX - btnEl.offsetLeft;
    /*x=horizontal     mouse pointers */
   const y = event.pageY - btnEl.offsetTop;
-/* y=vertical    mouse pointers*/
+
+  /* y=vertical    mouse pointers*/
   btnEl.style.setProperty("--xPos", x + "px");
   btnEl.style.setProperty("--yPos", y + "px");
 });
