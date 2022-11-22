30e5289a3e62d0542624a1f6457234bd05a4d150
Dina-Atef12 <dinaatef684@gmail.com>
2022-11-22 02:05:59 +0200

    link rel="stylesheet" href="style.css" /> = to link css with html

---
+++ main.js
@@ -0,0 +1,10 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (event) => {
+  const x = event.pageX - btnEl.offsetLeft;
+   /*x=horizontal     mouse pointers */
+  const y = event.pageY - btnEl.offsetTop;
+/* y=vertical    mouse pointers*/
+  btnEl.style.setProperty("--xPos", x + "px");
+  btnEl.style.setProperty("--yPos", y + "px");
+});
